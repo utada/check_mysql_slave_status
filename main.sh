ad90bc009e5952306fd79bfcf1680baca2011475
utada <utada4@gmail.com>
2012-08-22 19:16:19 +0900

    modified:   main.sh

--- main.sh
+++ main.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-while getopts "H:P:u:p:" Input;
-do
+while getopts "H:P:u:p:" Input; do
 	case ${Input} in
 	H)	host=${OPTARG};;
 	P)	port=${OPTARG};;
 	u)	user=${OPTARG};;
 	p)	password=${OPTARG};;
+	\?)	echo "Wrong option given. Please use options -H for host, -P for port, -u for user and -p for password"
+		exit 1
+		;;
   esac
 done
 
 # ${PWD##*/}
 SlaveReplStatus=`${PWD}/check_mysql_slavestatus.sh -H ${host} -P ${port} -u${user} -p${password}`
 
-if [ ${SlaveReplStatus:0:8} = "CRITICAL" ]; then
+if [ "${SlaveReplStatus:0:8}" = "CRITICAL" ]; then
   echo ${SlaveReplStatus}
   echo shutdown
   ShutdownResult=`mysqladmin -h ${host} -P ${port} -u ${user} --password=${password} -v shutdown`
